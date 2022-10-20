3ea3b2911b44fa57a881034bb8c11e030ad22348
JuhyunLim <87933367+userJu@users.noreply.github.com>
2022-10-21 06:55:18 +0900

    Test: 추가 the counter starts at 0 테스트

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("the counter starts at 0", () => {
+  // DOM에 컴포넌트를 렌더링
+  // 인자로 렌더링할 React 컴포넌트가 들어간다
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  // screen object를 이용해서 원하는 엘리먼트에 접근
+  const counterElement = screen.getByTestId("counter");
+
+  // id가 counter인 엘리먼트의 텍스트가 0인지 테스트
+  expect(counterElement).toBe(0);
 });
