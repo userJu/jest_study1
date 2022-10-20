09d76e1610c1106ad745b17221b157b6c19b60b7
JuhyunLim <87933367+userJu@users.noreply.github.com>
2022-10-21 07:27:25 +0900

    Test: 추가 + 버튼을 클릭하면 counter이 1이 된다

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import App from "./App";
 
 test("the counter starts at 0", () => {
@@ -23,4 +23,12 @@ test("plus button has correct text", () => {
   render(<App />);
   const plusButtonElement = screen.getByTestId("plus-button");
   expect(plusButtonElement).toHaveTextContent("+");
+});
+
+test("When the + button is pressed, the counter changes to 1", () => {
+  render(<App />);
+  const buttonElement = screen.getByTestId("plus-button");
+  fireEvent.click(buttonElement);
+  const counterElement = screen.getByTextId("counter");
+  expect(counterElement).toHaveTextContent(1);
 });
